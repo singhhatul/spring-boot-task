88bf4ae08cc36d168ec4e79a2cc904b70e4fffbb
atul.kumar1 <atul.kumar1@cgi.com>
2019-08-02 17:09:55 +0530

    changes in global exception

--- src/main/java/com/stackroute/trackservice/exceptions/GlobalException.java
+++ src/main/java/com/stackroute/trackservice/exceptions/GlobalException.java
@@ -8,16 +8,18 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+/* Spring support for a global @ExceptionHandler with the @ControllerAdvice annotation. This enables a mechanism that
+breaks away from the older MVC model and makes use of ResponseEntity along with the type safety and flexibility
+ of @ExceptionHandler:*/
 @ControllerAdvice
 public class GlobalException
         extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(value
-            = { TrackAlreadyExistsException.class, TrackNotFoundException.class })
+            = {TrackAlreadyExistsException.class, TrackNotFoundException.class})
     protected ResponseEntity<Object> handleConflict(
-            RuntimeException ex, WebRequest request) {
+            Exception ex, WebRequest request) {
         String bodyOfResponse = null;
-        return handleExceptionInternal(ex, bodyOfResponse,
-                new HttpHeaders(), HttpStatus.CONFLICT, request);
+        return new ResponseEntity<>(ex.getMessage(),HttpStatus.NOT_ACCEPTABLE);
     }
 }

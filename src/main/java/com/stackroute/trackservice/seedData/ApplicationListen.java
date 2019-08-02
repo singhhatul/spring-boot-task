b836e3dbe23f3e570a9489d7e2e043d3b971b8b1
atul.kumar1 <atul.kumar1@cgi.com>
2019-08-02 15:21:10 +0530

    comments added

--- src/main/java/com/stackroute/trackservice/seedData/ApplicationListen.java
+++ src/main/java/com/stackroute/trackservice/seedData/ApplicationListen.java
@@ -6,23 +6,29 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
-@Component
-public class ApplicationListen implements ApplicationListener<ContextRefreshedEvent>{
+
+    //this class implements Application Listener Interface
+    @Component
+    public class ApplicationListen implements ApplicationListener<ContextRefreshedEvent> {
     private TrackRepository trackRepository;
+
+    //contructor of class ApplicationListen
     @Autowired
     public ApplicationListen(TrackRepository trackRepository) {
         this.trackRepository = trackRepository;
     }
 
-
-
+    //onApplicationEvent method i made a object of track and added the values and save it will automatically save
+        // to database runtime
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
         System.out.println("Context Event Received");
-        Track track1=new Track(24,"atul","singh");
-        Track track2=new Track(25,"pushkar","shankar");
-        Track track3=new Track(26,"samresh","maiti");
-        Track track4=new Track(27,"shiv","prakash");
+        Track track1 = new Track(24, "atul", "singh");
+        Track track2 = new Track(25, "pushkar", "shankar");
+        Track track3 = new Track(26, "samresh", "maiti");
+        Track track4 = new Track(27, "shiv", "prakash");
+
+        //passing the track object
         trackRepository.save(track1);
         trackRepository.save(track2);
         trackRepository.save(track3);
